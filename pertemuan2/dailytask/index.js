3057a44f872e2d5ac6ad4c0092d811e791cf73a2
seeyzzaja <seeyzzaja@gmail.com>
2025-10-31 20:50:01 +0700

    arematika

---
+++ pertemuan2/dailytask/index.js
@@ -0,0 +1,30 @@
+let a = 90;
+let b = 10;
+let result = a + b;
+
+console.log(result);
+
+console.log(a > b ) 
+
+function sayhelo(){
+
+alert("saya naufal")
+
+
+}
+let c = 12;
+let d = 12; 
+console.log(c == b)
+console.log()
+
+
+// tugas studi kasus
+
+let e = " mie ayam "
+let f = " bakso "
+let g = 10000;
+let h = 25000;
+console.log("harga" + e + g)
+console.log( "harga" + f+ h)
+let total = g + h 
+console.log("total harga " + total)
