a020d2e0c763047226bdc89b23b0a32b936570fc
seeyzzaja <seeyzzaja@gmail.com>
2025-11-06 21:13:11 +0700

    add array

---
+++ pertemuan7/dailyTask/script.js
@@ -0,0 +1,17 @@
+let nama = ["naufal" , "arif", "muhammad", "dika"  ,"zikri"]
+
+nama.push("dimas")
+nama.unshift("rehan")
+nama.pop()
+nama.shift()
+nama.splice(1,0,"zik")
+console.log(nama)
+
+//evaluasi harian
+let belanja = []
+belanja.push("sayuran","buah buahan","minuman","makanan")
+belanja.pop()
+belanja.shift()
+belanja.splice(1,0 ,"makanan")
+console.log(belanja)
+console.log(belanja.length)
